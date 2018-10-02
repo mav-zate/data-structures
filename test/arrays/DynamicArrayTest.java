9a268a881ba04cdcdeb935dcafaaf21640a56ac9
Alzate <malzate@na.blkint.com>
2018-10-02 17:45:40 -0400

    Add junit 5 dependency

---
+++ test/arrays/DynamicArrayTest.java
@@ -0,0 +1,21 @@
+package arrays;
+
+
+/**
+ * Created by malzate on 10/2/2018.
+ */
+public class DynamicArrayTest {
+
+    // get
+    // when ()
+
+    // insert
+
+    // delete
+
+    // deleteAll
+
+    // add
+
+    // size
+}
