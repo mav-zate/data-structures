6cdad2b193e1bbafbcba49094de82b5d3460f2ef
Maverick Alzate <maverick.alzate@gmail.com>
2018-10-16 17:13:24 -0400

    Add test case for when SinglyLinkedList has values

--- test/linkedlists/SinglyLinkedListTest.java
+++ test/linkedlists/SinglyLinkedListTest.java
@@ -42,4 +42,59 @@ class SinglyLinkedListTest {
       Assertions.assertEquals(false, gotDeleted);
     }
   }
+
+  @Nested
+  @DisplayName("When has values")
+  class NotEmptyList {
+
+    @BeforeEach
+    void init() {
+      linkedList = new SinglyLinkedList<>();
+      linkedList.insert(3);
+      linkedList.insert(2);
+      linkedList.insert(1);
+    }
+
+    @Test
+    public void containsTest() {
+      Assertions.assertAll(() -> {
+        Assertions.assertEquals(false, linkedList.contains(5));
+        Assertions.assertEquals(true, linkedList.contains(2));
+      });
+    }
+
+    @Test
+    public void getTest() {
+      SinglyLinkedList.Node result = linkedList.get(2);
+      Assertions.assertEquals(2, result.getData());
+    }
+
+    @Test
+    @DisplayName("insert preserves order of insert")
+    public void insertTest() {
+      SinglyLinkedList.Node firstNode = linkedList.new Node<>(3, null);
+      SinglyLinkedList.Node secondNode = linkedList.new Node<>(2, firstNode);
+      SinglyLinkedList.Node thirdNode = linkedList.new Node<>(1, secondNode);
+
+
+      Assertions.assertAll(() -> {
+        Assertions.assertEquals(firstNode, linkedList.get(3));
+        Assertions.assertEquals(secondNode, linkedList.get(2));
+        Assertions.assertEquals(thirdNode, linkedList.get(1));
+      });
+    }
+
+    @Test
+    public void deleteTest() {
+      SinglyLinkedList.Node firstNodePostDeletion = linkedList.new Node<>(3, null);
+      SinglyLinkedList.Node secondNodePostDeletion =
+          linkedList.new Node<>(1, firstNodePostDeletion);
+
+      boolean gotDeleted = linkedList.delete(2);
+      Assertions.assertAll(() -> {
+        Assertions.assertEquals(true, gotDeleted);
+        Assertions.assertEquals(firstNodePostDeletion, secondNodePostDeletion.getNext());
+      });
+    }
+  }
 }
