d6c7fadc43f6d64fa588d017abcbfca466058b16
Maverick Alzate <maverick.alzate@gmail.com>
2018-11-04 18:34:08 -0500

    Add SelectionSort and test

---
+++ test/arrays/SelectionSortTest.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import comparison.AscendingOrder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import testutils.ArrayUtils;
+
+class SelectionSortTest {
+  SelectionSort<Integer> selectionSort;
+  DynamicArray<Integer> sortedArray;
+  DynamicArray<Integer> inputArray;
+
+  @BeforeEach
+  void init() throws Exception {
+    selectionSort = new SelectionSort<>();
+    sortedArray = ArrayUtils.initAscendingOrderArray(10);
+    inputArray = ArrayUtils.initUnsortedArray(10);
+  }
+
+  @Test
+  void testSort() {
+    selectionSort.sort(inputArray, new AscendingOrder<>());
+    Assertions.assertEquals(sortedArray, inputArray);
+  }
+}
