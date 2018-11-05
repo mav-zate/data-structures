07fe770023fab1ec91329b871d03975dbc83bfed
Alzate <malzate@na.blkint.com>
2018-11-05 17:51:49 -0500

    Add AvlTreeNode

---
+++ src/trees/AvlTreeNode.java
@@ -0,0 +1,66 @@
+package trees;
+
+public class AvlTreeNode<T extends Comparable<T>> {
+    private T key;
+    private int balanceFactor;
+    private int height;
+    private AvlTreeNode<T> parent;
+    private AvlTreeNode<T> leftChild;
+    private AvlTreeNode<T> rightChild;
+
+    public AvlTreeNode(T key) throws NullPointerException {
+      if (key == null) {
+        throw new NullPointerException();
+      }
+      this.key = key;
+      this.balanceFactor = 0;
+      this.height = 0;
+      this.parent = null;
+      this.leftChild = null;
+      this.rightChild = null;
+    }
+
+    public T getKey() {
+      return key;
+    }
+
+  public int getBalanceFactor() {
+      return balanceFactor;
+    }
+
+  public void setBalanceFactor(int balanceFactor) {
+    this.balanceFactor = balanceFactor;
+  }
+
+  public int getHeight() {
+      return height;
+    }
+
+  public void setHeight(int height) {
+    this.height = height;
+  }
+
+  public AvlTreeNode<T> getParent() {
+      return parent;
+    }
+
+    public void setParent(AvlTreeNode<T> parent) {
+      this.parent = parent;
+    }
+
+    public AvlTreeNode<T> getLeftChild() {
+      return leftChild;
+    }
+
+    public void setLeftChild(AvlTreeNode<T> leftChild) {
+      this.leftChild = leftChild;
+    }
+
+    public AvlTreeNode<T> getRightChild() {
+      return rightChild;
+    }
+
+    public void setRightChild(AvlTreeNode<T> rightChild) {
+      this.rightChild = rightChild;
+    }
+}
