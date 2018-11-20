6a818a479636035ba9a51f425e02d3fabedfdb73
mav-zate <maverick.alzate@gmail.com>
2018-11-20 11:45:07 -0500

    add TrieTest::insert

--- test/trees/TrieTest.java
+++ test/trees/TrieTest.java
@@ -1,10 +1,100 @@
 package trees;
 
-import static org.junit.Assert.*;
 
-/**
- * Created by malzate on 11/19/2018.
- */
+import arrays.CustomArray;
+import arrays.DynamicArray;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 public class TrieTest {
+  Trie trie;
 
+  void init() {
+    trie = new Trie();
+  }
+
+  @Nested
+  @DisplayName("::insert")
+  class TrieInsert {
+
+    @Test
+    @DisplayName("no-op on empty string")
+    void testInsertEmptyString() {
+      trie.insert("");
+    }
+
+    @Test
+    @DisplayName("inserts key with no prior prefixes")
+    void testKeyWithNoPriorPrefix() {
+      String exampleKey1 = "someString";
+      String exampleKey2 = "anotherString";
+      trie.insert(exampleKey1);
+      trie.insert(exampleKey2);
+      Assertions.assertEquals(true, trie.contains(exampleKey2));
+    }
+
+    @Test
+    @DisplayName("does not insert duplicate keys")
+    void testInsertNoDuplicateKeys() {
+      String exampleKey = "someString";
+      trie.insert(exampleKey);
+      trie.insert(exampleKey);
+      trie.delete(exampleKey);
+
+      Assertions.assertEquals(false, trie.contains(exampleKey));
+    }
+
+    @Test
+    @DisplayName("successfully branches off keys with same prefix")
+    void testInsertNewSuffixOnExistingPrefix() {
+      String prefix = "circum";
+      String suffix1 = "spect";
+      String suffix2 = "vent";
+
+      CustomArray<String> expectedResults = new DynamicArray<>((prefix + suffix1), (prefix + suffix2));
+
+      trie.insert(prefix + suffix1);
+      trie.insert(prefix + suffix2);
+      Assertions.assertEquals(expectedResults, trie.getAllKeysWithPrefix(prefix));
+    }
+  }
+
+//  @Nested
+//  @DisplayName("::delete")
+//  class TrieInsert {
+//
+//  }
+//
+//  @Nested
+//  @DisplayName("::contains")
+//  class TrieInsert {
+//
+//  }
+//
+//  @Nested
+//  @DisplayName("::getAllKeysWithPrefix")
+//  class TrieInsert {
+//
+//  }
+
+
+  @Test
+  public void testDelete() {
+    // if only one branch matches (no shared roots with other keys), delete whole branch
+    // if match suffix of shared root (and not prefix of other key), only delete relevant suffix not root
+    // if match prefix of other key, no delete. Just flip relevant terminal node
+    // if no match, no-op
+  }
+
+  @Test
+  public void testContains() {
+
+  }
+
+  @Test
+  public void testGetAllKeysStartingWith() {
+
+  }
 }
